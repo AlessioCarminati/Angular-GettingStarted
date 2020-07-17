c74fa020a00df44dc5a18a6d81c2d426487ea69c
Matteo Tomasulo <matteo.tomasulo@teorema.net>
2020-07-17 12:26:38 +0200

    implementata retrive di dettaglio prodotto

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -21,33 +21,34 @@ export class ProductListComponent implements OnInit {
   }
 
   set listFilter(value: string) {
-     this._listFilter = value;
-     this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
   }
 
   filteredProducts: IProduct[];
 
   products: IProduct[] = [];
 
-constructor(private productService: ProductService) {
-  this._listFilter = '';
-  this.errorMessage = '';
+  constructor(private productService: ProductService) {
+    this._listFilter = '';
+    this.errorMessage = '';
 
-}
-
-  ngOnInit() {
-     this.productService.getProducts()
-    .subscribe(
-      products => {this.products = products;
-      this.filteredProducts = this.products;
-    }, error => this.errorMessage = <any>error);
   }
 
-performFilter(filterBy: string): IProduct[] {
-  filterBy = filterBy.toLocaleLowerCase();
-  return this.products.filter((product: IProduct) =>
-    product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
-}
+  ngOnInit() {
+    this.productService.getProducts()
+      .subscribe(
+        products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+        }, error => this.errorMessage = <any>error);
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -13,7 +13,7 @@ export class ProductService {
 
   constructor(private http: HttpClient) {}
 
-  getProducts(): Observable<IProduct[]> {
+  getProducts(): Observable<IProduct[]> { 
 
     return this.http.get<IProduct[]>(this.productUrl).pipe(
       tap(data => console.log(JSON.stringify(data))), catchError(this.handleError)
